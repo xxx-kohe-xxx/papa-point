9de93e771e30668c7b63a7bcbe0335ea8014cd58
xxx-kohe-xxx <kohe.dekita@gmail.com>
2020-12-27 23:07:40 +0900

    ファイル読み込み&プレビュー表示

--- .eslintrc.js
+++ .eslintrc.js
@@ -152,6 +152,18 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
+
+    // jsx-a11y
+    'jsx-a11y/label-has-associated-control': [ 'error', {
+      'required': {
+        'some': [ 'nesting', 'id'  ]
+      }
+    }],
+    'jsx-a11y/label-has-for': [ 'error', {
+      'required': {
+        'some': [ 'nesting', 'id'  ]
+      }
+    }]
   },
   settings: {
     'import/parsers': {
--- src/components/RegistGift.tsx
+++ src/components/RegistGift.tsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-use-before-define
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
   Button,
   Card,
   CardHeader,
+  // CardMedia,
   makeStyles,
   TextField,
 } from '@material-ui/core';
@@ -18,20 +19,36 @@ const useStyles = makeStyles({
     width: '90%',
     margin: `${cssConst.spaceS}px 5%`,
   },
+  input: {
+    display: 'none',
+  },
+  media: {
+    backgroundSize: 'cover',
+    height: '25vh',
+    display: 'block',
+    margin: '0 auto',
+  },
   button: {
     width: '90%',
     display: 'block',
     margin: `${cssConst.spaceS}px auto ${cssConst.spaceM}px`,
+    textAlign: 'center',
   },
 });
 
 export const RegistGift: FC = () => {
   const classes = useStyles();
+  const [image, setImage] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event != null && event.target != null && event.target.files != null) {
+      const loadedImages = URL.createObjectURL(event.target.files[0]);
+      setImage(loadedImages);
+    }
+  };
+
   const registrationGift = () => {
     console.log('登録します');
-  };
-  const uploadFile = () => {
-    console.log('画像アップロード');
   };
 
   return (
@@ -57,13 +74,19 @@ export const RegistGift: FC = () => {
         }}
         variant="outlined"
       />
-      <Button
-        className={classes.button}
-        variant="outlined"
-        onClick={uploadFile}
-      >
-        画像を選択する
-      </Button>
+      {!image ? false : <img className={classes.media} src={image} alt="" />}
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="registration-image"
+        type="file"
+        onChange={handleChange}
+      />
+      <label htmlFor="registration-image">
+        <Button className={classes.button} variant="outlined" component="span">
+          画像を選択する
+        </Button>
+      </label>
       <Button
         className={classes.button}
         variant="contained"

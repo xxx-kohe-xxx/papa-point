a2a599a5504cd4ded29f6f57684160f9f7ab5634
xxx-kohe-xxx <kohe.dekita@gmail.com>
2021-06-26 05:30:14 +0900

    ファイルをtsxに変更

---
+++ papa-point/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>PaPaPoint</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}

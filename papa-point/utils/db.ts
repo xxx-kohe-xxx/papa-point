d43cdbce4bf0244d81063f18e3927bc19f84ba88
xxx-kohe-xxx <kohe.dekita@gmail.com>
2021-07-19 05:58:10 +0900

    合計ポイントをDBから取得するメソッド作成

--- papa-point/utils/db.ts
+++ papa-point/utils/db.ts
@@ -8,3 +8,15 @@ export const addUser = async (authUser: any) => {
     .set({...authUser}, {merge: true});
   return resp;
 };
+
+export const getTotalPoint = async (name: string) => {
+  const snapshot = await firebase
+    .firestore()
+    .collection('total_point')
+    .doc(name)
+    .get();
+  const totalPoint = snapshot.get('point');
+  console.log(totalPoint);
+
+  return totalPoint;
+};
